f4b6ecccc654b8d22a12e944ff85f506ecfd0310
Taylor <taylor@christie.pw>
2017-09-29 20:31:21 -0600

    Main Scaffolding

---
+++ .provision/dependencies.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y git build-essential curl nginx lib32stdc++6 lib32z1
+
+echo "cd /vagrant" >> /home/vagrant/.bashrc
+chown -R vagrant:vagrant /home/vagrant --quiet
